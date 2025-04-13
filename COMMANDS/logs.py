fa9a8ccac59d325a4b6f410381c7635f12e6225d
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-04-13 13:27:19 +0530

    Create logs.py

---
+++ COMMANDS/logs.py
@@ -0,0 +1,40 @@
+import io
+from pyrogram import *
+from MAIN.__main__ import Officer as bot
+from MAIN import HANDLERS
+from config import OWNER_ID
+from config import SUDO
+import traceback
+from subprocess import getoutput as run
+from pyrogram.enums import ChatAction
+
+@bot.on_message(filters.command(["logs", "log"], prefixes=HANDLERS))
+async def logs(_, message):
+    if message.from_user.id == OWNER_ID or message.from_user.id in SUDO:
+        print("")
+    else:
+        return
+    run_logs = run("tail log.txt")
+    text = await message.reply_text("`Getting logs...`")
+    await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+    await message.reply_text(f"```shell\n{run_logs}```")
+    await text.delete()
+
+
+@bot.on_message(filters.command(["flogs", "flog"], prefixes=HANDLERS))
+async def logs(_, message):
+    if message.from_user.id == OWNER_ID or message.from_user.id in SUDO:
+      print("")
+    else:
+        return
+    run_logs = run("cat log.txt")
+    text = await message.reply_text("`Sending Full logs...`")
+    await bot.send_chat_action(message.chat.id, ChatAction.UPLOAD_DOCUMENT)
+    with io.BytesIO(str.encode(run_logs)) as logs:
+        logs.name = "log.txt"
+        await message.reply_document(
+            document=logs,
+        )
+    await text.delete()
+
+
