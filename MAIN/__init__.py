63b151ee1224741546854cc61d4bc74ec9ebdc64
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-04-13 09:39:49 +0530

    Create __init__.py

---
+++ MAIN/__init__.py
@@ -0,0 +1,17 @@
+import config 
+from pyrogram import Client, filters 
+import logging 
+import os 
+import sys
+
+
+if not API_ID or API_HASH or TOKEN:
+  print("❌ API_ID, HASH nad TOKEN must be in environment varaibles") 
+
+
+Raijin = Client(
+  api_id=config.API_ID, 
+  api_hash=config.API_HASH, 
+  bot_token=config.TOKEN, 
+  plugins=dict(root="COMMAND/")
+) 
