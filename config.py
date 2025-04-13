36233b309e84ff0e96b08c3dd511f0c46a7b09c0
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-04-13 11:15:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ API_ID = int(os.getenv("API_ID", 27548865))
 API_HASH = os.getenv("API_HASH,", "db07e06a5eb288c706d4df697b71ab61") 
 TOKEN = os.getenv("TOKEN", "7576952586:AAGVzpc91nY8fhsiUkC-n9uyHScKh2ioM4E") 
 OWNER_ID = int(os.getenv("OWNER_ID", 6239769036)) 
-SUDO = [6239769036, 
+SUDO = [6239769036, 7080282783]
