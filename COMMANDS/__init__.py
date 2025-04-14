ceba7afc0122bfcc4596cd391c7ffc7d36ee9194
𝙺𝚄𝚁𝙾-𝚁𝙰𝙸𝙹𝙸𝙽 𝙓 黒雷神 <sufyan532011@gmail.com>
2025-04-14 08:46:35 +0530

    Update __init__.py

--- COMMANDS/__init__.py
+++ COMMANDS/__init__.py
@@ -1,9 +1,17 @@
 #HERE WE CAN MAKE ALL COMMANDS WORKING 
 
+import glob
+from os.path import basename, dirname, isfile
 
-from .about import *
-from .handle_abuses import *
-from .start import * 
-from .logs import * 
+def __list_all_modules():
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
 
+    all_modules = [
+        basename(f)[:-3]
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+    return all_modules
 
+ALL_MODULES = sorted(__list_all_modules())
+__all__ = ALL_MODULES + ["ALL_MODULES"]
